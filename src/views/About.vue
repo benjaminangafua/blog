826f5ec855b99e359994fbd41d3879fb05cc43f5
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2023-02-10 20:27:44 +0000

    add an about me page

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,11 +4,27 @@
 </script>
 
 <template> 
+<div class="col-2 m-4">
+    <div class="txt">
+        <div class="heading" >About Me</div>
+        <div class="email">
+            <div><b>Email</b> </div>
+            <div class="orange">benjaminangafua@gmail.com</div>
+        </div>
+        <div class="Role">
+            <div><b>Role</b> </div>
+            <div>Developer</div>
+        </div>
+        <div class="Phone">
+            <div><b>Phone</b> </div>
+            <div class="orange">(+231) 880971-128</div>
+        </div>
+        <div class="btn"><button>GET IN TOUCH</button></div>
+    </div>
+    <div class="about-img"><img src="/images/about-me.jpeg" alt=""></div>
+</div>
 </template>
 
 <style scoped>
 
-#app{
-    background-color: teal;
-}
 </style>
